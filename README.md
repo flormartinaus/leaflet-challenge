9fb0a5079f372ab3d5d0cacc181cdf0e540bcc87
Maria Florencia Martin <126949411+flormartinaus@users.noreply.github.com>
2023-07-09 11:14:56 +0800

    updated code and readme

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # leaflet-challenge
  
+ This project aims to provide a visual representation of earthquake data collected by the United States Geological Survey (USGS) using interactive maps. The goal is to develop a user-friendly tool that effectively communicates the magnitude, location, and depth of earthquakes to the public and government organizations.
+
+## Background
+The USGS is responsible for collecting and analyzing data related to natural hazards, environmental health, and climate change. With a massive amount of earthquake data collected daily from various locations worldwide, the USGS requires a comprehensive visualization solution to present this information in a meaningful way.
+
+## Objective
+The objective of this project is to develop a set of tools that will allow the USGS to visualize their earthquake data effectively. By creating interactive maps, the project aims to educate the public and government organizations about the challenges our planet faces due to seismic activities. Additionally, the project aims to assist the USGS in securing funding for their ongoing research and data collection efforts.
+
+## Technologies Used
+The following technologies were utilized in this project:
+
+Leaflet: A JavaScript library for interactive maps.
+HTML: The standard markup language for creating web pages.
+CSS: Cascading Style Sheets for styling the web pages.
+JavaScript: The programming language used for implementing interactive functionalities.
+D3: A JavaScript library for manipulating documents based on data.
+
+## Resources
+The project utilized various resources for development, including:
+
+Class exercises: Incorporating concepts and techniques learned during the course.
+Stack Overflow: Community-driven question and answer platform for troubleshooting.
+ASK BCS: Consultation and guidance from BCS tutors.
+Tutorials: Online tutorials and documentation for learning and implementing specific features.
+YouTube tutorials: Video tutorials for understanding and applying relevant techniques.
+Leaflet plug-ins: Additional plug-ins for extending the functionality of the Leaflet library.
+Usage
+To visualize the earthquake data, follow these steps:
+
+Access the project repository: GitHub Repository
+Clone the repository to your local machine.
+Open the project in a web browser.
+The interactive map will display the earthquake data, showcasing the magnitude, location, and depth of each earthquake.
+Click on a marker to view additional details about each earthquake.
+
+## Example
+
+![Example Screenshot](/Starter_Code/example.PNG)
+
+## Credits
+The success of this project is attributed to the following resources:
+
+USGS: Provider of earthquake data and project inspiration.
+Leaflet documentation: Official documentation for the Leaflet library.
+CSS-Tricks: Online resource for CSS techniques and best practices.
+MDN Web Docs: Comprehensive documentation for web technologies.
+Open-source community: Developers contributing to the development of relevant libraries and tools.
+
+## License
+This project is licensed under the MIT License.
+
+Please note that the earthquake data used in this project is sourced from the USGS and may be subject to their terms and conditions.
--- Starter_Code/static/js/logic.js
+++ Starter_Code/static/js/logic.js
@@ -1,39 +1,94 @@
-//store the URL for the GeoJSON data
-const url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
 
-// Adding a Leaflet tile layer.
-let streets = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+// Create the map object
+const myMap = L.map("map", {
+    center: [28.304381, -196.526448],
+    zoom: 2.5
+  });
+  
+  // Adding the tile layer for Open Street Map
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-});
-
-// Creating a Leaflet map object.
-let myMap = L.map("map", {
-    center: [37.09, -95.71],
-    zoom: 5,
-    layers: [streets]
-});
-
-//defining basemaps
-let baseMaps = {
-    "streets": streets
-};
-
-//defining the earthquake layergroup
-let earthquake_data = new L.LayerGroup();
-
-//defining the overlays and link the layergroups to separate overlays
-let overlays = {
-    "Earthquakes": earthquake_data,
-};
-
-//adding a control layer and pass in baseMaps and overlays
-L.control.layers(baseMaps, overlays).addTo(myMap);
-
-//this function will style all the earthquake areas on the map
-function earthquakeStyle(feature) {
-    return {
-        color: chooseColor(feature.geometry.coordinates[2]),
-        radius: chooseRadius(feature.properties.mag),
-        fillColor: chooseColor(feature.geometry.coordinates[2])
+  }).addTo(myMap);
+  
+  // Definite URL variable of geoJSON link
+  const baseURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson?";
+  
+  // Create a function for the radius of markers related to magnitude
+  function calculateRadius(magnitude) {
+    return magnitude * 3;
+  }
+  
+  // Create a function for coloring the markers according to depth
+  function getColor(depth) {
+    if (depth > 90) {
+      return "darkred";
     }
-};
+    if (depth > 70) {
+      return "red";
+    }
+    if (depth > 50) {
+      return "orange";
+    }
+    if (depth > 30) {
+      return "yellow";
+    }
+    if (depth > 10) {
+      return "green";
+    }
+    return "blue";
+  }
+  
+  // Call in the data and visualize on map
+  d3.json(baseURL).then(function (data) {
+    L.geoJSON(data, {
+      pointToLayer: function (feature, latLng) {
+        return L.circleMarker(latLng);
+      },
+  
+      // Bind Popups to each feature
+      onEachFeature: function (feature, layer) {
+        layer.bindPopup("<h3>Location: " + feature.properties.place + "</h3><hr><p>Magnitude: " + feature.properties.mag + "</p><p>Depth: " + feature.geometry.coordinates[2]);
+      },
+  
+      // Style markers
+      style: function (feature, layer) {
+        return {
+          radius: calculateRadius(feature.properties.mag),
+          opacity: 1, // Increase the marker opacity
+          fillOpacity: 0.8, // Increase the fill opacity
+          color: getColor(feature.geometry.coordinates[2]), // Set the stroke color based on depth
+          weight: 1.5 // Increase the stroke width
+        };
+      }
+  
+    }).addTo(myMap);
+  
+    // Create a legend control object for depth
+    const depthLegend = L.control({ position: "bottomright" });
+    depthLegend.onAdd = function (map) {
+      const div = L.DomUtil.create("div", "info legend");
+      const grades = [-10, 10, 30, 50, 70, 90];
+      const colors = [
+        "blue",
+        "green",
+        "yellow",
+        "orange",
+        "red",
+        "darkred"
+      ];
+      div.innerHTML += "<h4>Depth Legend</h4>";
+      // Loop through our intervals and generate a label with a colored square for each interval.
+      for (let i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+          '<i style="background:' + colors[i] + '"></i> ' +
+          grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+      }
+      return div;
+    };
+    depthLegend.addTo(myMap);
+  
+  });
+  
+  
+
+  
